8420170853fe47ecada291e0caf597d52ecff099
Chirag <chiragcse05@gmail.com>
2026-07-19 20:11:42 +0530

    Time: 3 ms (88.84%), Space: 23.9 MB (30.62%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,31 @@
 class Solution {
-    static bool cmp(vector<int>&a,vector<int>&b){
-        return a[0]<b[0];
-    }
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end(),cmp);
-
-        
-
-        int i=0;
-        while(i<intervals.size()){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            while(i + 1 < intervals.size() && intervals[i+1][0] <= end){
-                end = max(end, intervals[i + 1][1]);
-                i++;
-            }
-
-            ans.push_back({start,end});
-            i++;
-
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        if (a[0] != b[0]) {
+            return a[0] < b[0];
         }
-        return ans;
+        return a[1] < b[1];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        vector<vector<int>> ans;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int n= intervals.size();
 
+        int i=1;
+        while(i<n){
+            if(intervals[i][0]<=end){
+                end = max(intervals[i][1],end);
+            }else{
+                ans.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+            i++;
+        }
+        ans.push_back({start,end});
+
+        return ans;
     }
 };
