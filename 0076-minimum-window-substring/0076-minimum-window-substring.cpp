09506c438e41e0917983bacf5902b46f8fcaf09a
Chirag <chiragcse05@gmail.com>
2026-07-23 05:44:14 +0530

    Time: 7 ms (61.03%), Space: 11.5 MB (43.54%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> freq(128, 0);
+        unordered_map<char,int>idx;
+        unordered_map<char,int>mp;
 
-        for (char c : t)
-            freq[c]++;
-
-        int left = 0;
-        int start = 0;
-        int minLen = INT_MAX;
-        int count = t.size();
-
-        for (int right = 0; right < s.size(); right++) {
-
-            if (freq[s[right]] > 0)
-                count--;
-
-            freq[s[right]]--;
-
-            while (count == 0) {
-
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    start = left;
-                }
-
-                freq[s[left]]++;
-
-                if (freq[s[left]] > 0)
-                    count++;
-
-                left++;
-            }
+        for(auto a :t){
+            mp[a]++;
         }
 
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+        int left =0;
+        int count =t.size();
+        int minlen =INT_MAX;
+        int start=0;
+        
+        for(int right = 0;right<=s.size();right++){
+            char a = s[right];
+            if(mp[a]>0){
+                count--;
+            }
+            mp[a]--;
+                
+                while(count==0){
+                    if(minlen > right-left+1){
+                        minlen = right-left+1;
+                        start = left;
+                    }
+
+                    mp[s[left]]++;
+                    if(mp[s[left]]>0){
+                        count++;
+                    }
+                    left++;
+                }
+
+
+        }
+        return minlen==INT_MAX?"":s.substr(start,minlen);
     }
 };
