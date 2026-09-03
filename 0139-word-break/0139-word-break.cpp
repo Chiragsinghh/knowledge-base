b7b43c5f8e34684f5b2eef016a00bf9557e398f7
Chirag <chiragcse05@gmail.com>
2026-09-03 13:49:48 +0530

    Time: 27 ms (8.23%), Space: 20.1 MB (10.25%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool check(string s, vector<string>& wordDict){
-        for(auto i:wordDict){
+        for(auto i :wordDict){
             if(s==i){
                 return true;
             }
@@ -10,40 +10,27 @@ public:
         return false;
     }
     bool solve(string s, vector<string>& wordDict,int start,vector<int>&dp){
-        if(start == s.size()) return true;
+        if(start>= s.size()){
+            return true;
+        }
 
         if(dp[start] != -1) return dp[start];
 
         string word = "";
         bool flag = false;
 
-        for(int i=start;i<s.size() ;i++){
-            word = word+s[i];
+        for(int i=start ;i<s.size();i++){
+            word += s[i];
             if(check(word,wordDict)){
                 flag = flag || solve(s,wordDict,i+1,dp);
             }
         }
-        dp[start] = flag;
-        return dp[start];
-    }
-    bool solvedu(string s, vector<string>& wordDict,vector<bool>& dp){
-        for(int start=s.size() -1 ;start>=0 ;start--){
-            string word = "";
-            bool flag = false;
-            for(int i= start ;i<s.size();i++){
-                word=word+s[i];
-                if(check(word,wordDict)){
-                flag = flag || dp[i+1];
-                }
-            dp[start] = flag;
-            }
-        }
 
-        return dp[0];
+        return dp[start]=flag;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool>dp(s.size()+1,true);
-        return solvedu(s,wordDict,dp);
-
+        int start =0;
+        vector<int> dp(s.size(), -1);
+        return solve(s,wordDict,start,dp);
     }
 };
